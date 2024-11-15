571c2d05ce4e1786cc71d10a7ca351529db92397
Beastan <dhanoop.karunakaran@gmail.com>
2024-11-15 16:05:00 +1100

    changes

--- run.bash
+++ run.bash
@@ -2,6 +2,6 @@
 # Run below step one by one in terminal
 sudo docker rmi -f $(sudo docker images -f "dangling=true" -q)
 docker build -t actor_critic .
-docker run --net host --gpus all -it -v /home/$(whoami)/Documents/projects/actor-critic-implementation:/app actor_critic
+docker run --net host --gpus all -it -v /home/$(whoami)/Documents/projects/advantage-actor-critic-pytorch:/app actor_critic
 
 
--- train.py
+++ train.py
@@ -6,10 +6,11 @@ from critic import Critic
 import yaml
 from utility import logger_helper
 from torch.distributions import Categorical
+import numpy as np
 
 
 # Ref: https://github.com/nikhilbarhate99/Actor-Critic-PyTorch
-# Ref: https://github.com/nikhilbarhate99/Actor-Critic-PyTorch/blob/master/model.py
+# Ref: https://github.com/Lucasc-99/Actor-Critic/blob/master/src/a2c.py
 
 class Train:
     def __init__(self):
@@ -23,19 +24,28 @@ class Train:
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.actor = Actor(self.env.observation_space.shape[0], self.env.action_space.n)
         self.critic = Critic(self.env.observation_space.shape[0])
+        
 
     def run(self):
         torch.manual_seed(self.cfg['train']['random_seed'])
-        optim_1 = optim.Adam(self.actor.parameters(), lr=self.cfg['train']['lr'], betas=self.cfg['train']['betas'])
-        optim_2 = optim.Adam(self.critic.parameters(), lr=self.cfg['train']['lr'], betas=self.cfg['train']['betas'])
+        actor_optim = optim.Adam(self.actor.parameters(), lr=self.cfg['train']['lr'], betas=self.cfg['train']['betas'])
+        critic_optim = optim.Adam(self.critic.parameters(), lr=self.cfg['train']['lr'], betas=self.cfg['train']['betas'])
+        avg_reward = []
+        actor_losses = []
+        critic_losses = []
         for episode in range(self.cfg['train']['n_epidode']):
             rewards = []
+            log_probs = []
+            advantages = []
+
             state = self.env.reset()
             # Converted to tensor
             state = torch.FloatTensor(state[0])
             self.logger.info(f"--------Episode: {episode} started----------")
+            actor_optim.zero_grad()
+            critic_optim.zero_grad()
             # loop through timesteps
-            for t in range(self.cfg['train']['n_timesteps']):
+            for i in range(self.cfg['train']['n_timesteps']):
                 # The actor layer output the action probability as the actor NN has softmax in the output layer
                 action_prob = self.actor(state)
                 # categorical function can  give categorical distribution from softmax probability or from logits(no softmax layer in output) with logits as attribute 
@@ -61,6 +71,53 @@ class Train:
                 # Compute the state-value for next state
                 next_state = torch.FloatTensor(next_state)
                 v_st_plus_1 = self.critic(next_state)
+
+                # Get the advantage from r(s_t,a_t)+v_s_t+1 - v_s_t
+                adv = reward+(self.cfg['train']['gamma']*v_st_plus_1.detach().numpy().item()) - v_st.detach().numpy().item()
+
+                # String the value for loss computation
+                rewards.append(reward)
+                log_probs.append(log_prob)
+                advantages.append(adv)
+
+                #assign next state as current state
+                state = next_state
+
+                if done:
+                    self.logger.info('Iteration: {}, Score: {}'.format(episode, i))
+                    break
+            
+            avg_reward.append(np.mean(rewards))        
+            advantages = torch.tensor(advantages)
+            rewards = torch.tensor(rewards)
+            log_probs = torch.cat(log_probs)
+
+            # Same as cross entropy loss for where cross entropy loss is multiplied with Advantage function. formuala:  sum of log pi_theta(a_t, s_t) * Advantage
+            actor_loss = -(log_probs * advantages.detach()).mean()
+            actor_losses.append(actor_loss.detach().numpy().item())
+
+            # Some form of advantage function is used to commpute the loss of critic
+            critic_loss = advantages.pow(2).mean()
+            
+            # Need to make required grad true to compute the loss
+            critic_loss.requires_grad=True
+            critic_losses.append(critic_loss.detach().numpy().item())
+
+            # Compute gradient
+            actor_loss.backward()
+            critic_loss.backward()
+
+            # optimisation
+            actor_optim.step()
+            critic_optim.step()
+
+        print(actor_losses)
+        torch.save(self.actor, 'model/actor.pkl')
+        torch.save(self.critic, 'model/critic.pkl')
+        self.env.close()
+
+
+
                 
 
                 
